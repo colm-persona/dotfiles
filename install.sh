6a1dc0945b11ca7911d053a49213f61f47333f45
Colm Lang <colmlang@Colms-Laptop.local>
2024-07-02 10:26:54 -0700

    source

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")";
-cat .bash_profile >> ~/.bashrc
-source ~/.bashrc
+cat .bash_profile >> ~/.bash_profile
+source ~/.bash_profile
+echo "source ~/.bash_profile" >> ~/.bashrc
+echo "#from install" >> ~/.bashrc
