ef427da56c3717c2933fed357336e98eff4afd25
Colm Lang <colmlang@Colms-Laptop.local>
2025-04-22 14:55:46 -0700

    .

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,7 +6,10 @@ vim.keymap.set('n', '<leader>;f', builtin.find_files, {})
 -- find files TRACKED BY GIT in directory that you opened vim in --
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 
--- grep string in directory that you opened vim in --
-vim.keymap.set('n', '<leader>ps', function()
+-- grep --
+vim.keymap.set('n', '<leader>gl', function() builtin.grep_live(); end)
+vim.keymap.set('n', '<leader>gs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>gr', builtin.resume, { noremap = true, silent = true, desc = "Resume" })
+
