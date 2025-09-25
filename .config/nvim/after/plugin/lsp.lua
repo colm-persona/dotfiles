597ba160dd4f1992725c05a1d34b516e3409c498
Colm Lang <colm@persona-ai.ai>
2025-09-25 22:14:24 +0000

    nvim 0.11 config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,3 +1,61 @@
+require('mason').setup()
+
+vim.diagnostic.config({ virtual_lines =  true })
+
+local configs = {}
+
+for _, v in ipairs(vim.api.nvim_get_runtime_file('lsp/*', true)) do
+  local name = vim.fn.fnamemodify(v, ':t:r')
+  configs[name] = true
+end
+
+vim.lsp.enable(vim.tbl_keys(configs))
+
+vim.o.winborder = 'rounded'
+
+local augroup = vim.api.nvim_create_augroup
+local colmgroup = augroup('colm', {})
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = colmgroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
+local cmp = require('blink.cmp')
+cmp.setup({})
+
+-- Hide copilot on suggestion
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
+  callback = function()
+    require("copilot.suggestion").dismiss()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
+
+--[[
+--
+--
 local lsp = require('lsp-zero').preset("recommended")
 
 lsp.ensure_installed({
@@ -117,3 +175,4 @@ vim.api.nvim_create_autocmd(
         end,
     }
 )
+]]--
